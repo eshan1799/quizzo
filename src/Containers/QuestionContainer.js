5e35df5f4a0f7ac2797d6ddee62009841c3d41a0
yassine <yassine.benlamkadem@gmail.com>
2020-08-17 14:54:19 +0100

    changeQuestionHandler debugging

--- src/Containers/QuestionContainer.js
+++ src/Containers/QuestionContainer.js
@@ -117,24 +117,35 @@ class QuestionContainer extends Component {
     }
     
     changeQuestionHandler = () => {
-        if (this.state.questionCount + 1 < this.state.players[this.state.playerCount].questions.length) {
-            console.log(`Question: ${this.state.questionCount + 1}`)
-            this.setState(prev => ({ questionCount: prev.questionCount + 1 }))
-        } else {
+        //if (this.state.players[this.state.playerCount] !== undefined) {
+        if (this.state.questionCount + 1 < this.state.players[this.state.playerCount].questions.length ) {
+            // console.log(`Question: ${this.state.questionCount}`)
+            this.setState(prev => ({ questionCount: ++prev.questionCount }), () => console.log(`Question: ${this.state.questionCount}`))
+        } else if (this.state.playerCount + 1 < this.state.players.length) {
             this.setState({ questionCount: 0 })
-            if (this.state.playerCount+1 < this.state.players.length) {
-                console.log(`Player: ${this.state.playerCount+1}`)
-                this.setState(prev => { return ({ playerCount: prev.playerCount + 1 }) })
-            } else {
+            
+            this.setState(prev => { return ({ playerCount: ++prev.playerCount }), console.log(`Player: ${this.state.playerCount}`) })
+        }
+                //console.log(`Player: ${this.state.playerCount}`)
+        else {
                 console.log("end")
             }
-        }   
+        
+            //console.log(`Question Async?: ${this.state.questionCount}`)
+        //}   
+        //console.log(`Question Async2?: ${this.state.questionCount}`)
     }
 
     render() {
         return (
             <div>
-                <QuestionComponent onclick={this.changeQuestionHandler}/>
+                <QuestionComponent 
+                onclick={this.changeQuestionHandler} 
+                player={this.state.players[this.state.playerCount]} 
+                question={this.state.players[this.state.playerCount].questions[this.state.questionCount].question}
+                correct_answer={this.state.players[this.state.playerCount].questions[this.state.questionCount].correct_answer}
+                incorrect_answer={this.state.players[this.state.playerCount].questions[this.state.questionCount].incorrect_answers}
+                />
             </div>
         )
     }
